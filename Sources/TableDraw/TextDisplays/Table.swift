7c83776cbf637c39ab3acdba45dfd7339835adb3
Juri Pakaste <juri@juripakaste.fi>
2022-02-27 08:46:20 +0200

    Only split header once

--- Sources/TableDraw/TextDisplays/Table.swift
+++ Sources/TableDraw/TextDisplays/Table.swift
@@ -14,15 +14,16 @@ public struct Table: TextDisplay {
         var columnWidths = [Int](repeating: 0, count: self.columns.count)
         var hasHeaderTopBorder = false
         var hasHeaderBottomBorder = false
+        let headerLines = self.columns.map { $0.header?.title.split(separator: "\n") ?? [] }
 
         var headerHeight: Int = 0
         for (columnIndex, column) in zip(0..., self.columns) {
             guard let header = column.header else { continue }
-            let headerLines = header.title.split(separator: "\n")
-            let headerContentWidth = headerLines.reduce(0) { maxWidth, line in max(maxWidth, line.count) }
+            let lines = headerLines[columnIndex]
+            let headerContentWidth = lines.reduce(0) { maxWidth, line in max(maxWidth, line.count) }
             let headerWidth = headerContentWidth + header.leadingMargin.count + header.trailingMargin.count
             columnWidths[columnIndex] = headerWidth
-            let contentHeight = max(headerLines.count, header.minHeight)
+            let contentHeight = max(lines.count, header.minHeight)
             let height = contentHeight + header.decorationHeight
             headerHeight = max(headerHeight, height)
             hasHeaderTopBorder = hasHeaderTopBorder || header.topBorder != nil
@@ -69,8 +70,8 @@ public struct Table: TextDisplay {
                 }
                 continue
             }
-            let headerLines = header.title.split(separator: "\n")
-            let paddedLines = header.verticalPadding.apply(lines: headerLines)
+            let lines = headerLines[columnIndex]
+            let paddedLines = header.verticalPadding.apply(lines: lines)
             let verticallyAlignedLines = header.verticalAlignment.apply(
                 text: paddedLines,
                 height: headerHeight - (hasHeaderTopBorder ? 1 : 0) - (hasHeaderBottomBorder ? 1 : 0)
