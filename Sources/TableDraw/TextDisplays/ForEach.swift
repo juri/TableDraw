f6a5b6c6277c6374f03db847ad3520e277e308a0
Juri Pakaste <juri@juripakaste.fi>
2022-02-27 14:05:43 +0200

    Remove external name from ForEach's source

--- Sources/TableDraw/TextDisplays/ForEach.swift
+++ Sources/TableDraw/TextDisplays/ForEach.swift
@@ -1,7 +1,7 @@
 public struct ForEach: TextDisplay {
     var content: [TextDisplay]
 
-    public init<Source: Sequence>(source: Source, @ForEachBuilder content: (Source.Element) -> [TextDisplay]) {
+    public init<Source: Sequence>(_ source: Source, @ForEachBuilder content: (Source.Element) -> [TextDisplay]) {
         self.content = source.flatMap(content)
     }
 
