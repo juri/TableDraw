90a53afa0aa48526f774146e39f1637baf1823d5
Juri Pakaste <juri@juripakaste.fi>
2022-03-03 21:13:23 +0200

    Support if in TableDisplayBuilder

--- Sources/TableDraw/Builders/TableDisplayBuilder.swift
+++ Sources/TableDraw/Builders/TableDisplayBuilder.swift
@@ -1,6 +1,22 @@
 @resultBuilder
 public enum TableDisplayBuilder {
-    public static func buildBlock(_ components: TextDisplay...) -> [TextDisplay] {
+    public typealias Component = [TextDisplay]
+    public typealias Expression = TextDisplay
+
+    public static func buildBlock(_ components: Component...) -> Component {
+        components.flatMap { (tds: [TextDisplay]) -> [TextDisplay] in
+            tds.flatMap { td -> [TextDisplay] in
+                switch td {
+                case let forEach as ForEach:
+                    return forEach.content
+                default:
+                    return [td]
+                }
+            }
+        }
+    }
+
+    public static func buildBlock(_ components: Expression...) -> Component {
         components.flatMap { td -> [TextDisplay] in
             switch td {
             case let forEach as ForEach:
@@ -11,7 +27,26 @@ public enum TableDisplayBuilder {
         }
     }
 
-    public static func buildBlock(_ components: ForEach...) -> [TextDisplay] {
-        components.flatMap(\.content)
+    public static func buildBlock() -> Component {
+        []
+    }
+
+    public static func buildExpression(_ expression: Expression) -> TableDisplayBuilder.Component {
+        [expression]
+    }
+
+    public static func buildOptional(_ component: Component?) -> Component {
+        guard let component = component else { return [] }
+        return component
+    }
+}
+
+public struct Group<Content> where Content: TextDisplay {
+    let children: [Content]
+}
+
+extension Group: TextDisplay {
+    public var stringValue: String {
+        self.children.map(\.stringValue).joined()
     }
 }
